29c75d0814789ba90af7146aa67e676d34cc5cf0
Justin Young <justinyoung371@yahoo.com>
2020-09-22 15:57:49 -0700

    Updates

---
+++ precache-manifest.64eef532c5bc5506b210a56d22d6ad66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db962fb93c43cc9bf9a942c04153f39c",
+    "url": "/hacker-news-clone/index.html"
+  },
+  {
+    "revision": "0a95d1ff14f1c18ffcf8",
+    "url": "/hacker-news-clone/static/js/2.01a2b292.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/hacker-news-clone/static/js/2.01a2b292.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5d2672a850ec7a71f46",
+    "url": "/hacker-news-clone/static/js/main.e937de63.chunk.js"
+  },
+  {
+    "revision": "c9a3baf2b0bc3fd6620c",
+    "url": "/hacker-news-clone/static/js/runtime-main.033094b6.js"
+  }
+]);
